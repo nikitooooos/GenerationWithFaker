15c25bd9bf026c360d4811326a43692f2796cd34
nikitooooos <nikita.postnikov.00@gmail.com>
2023-11-10 15:48:43 +0300

    remove faker from RandomUtils

--- src/test/java/utils/RandomUtils.java
+++ src/test/java/utils/RandomUtils.java
@@ -1,13 +1,8 @@
 package utils;
 
-import com.github.javafaker.Faker;
-
 import java.util.concurrent.ThreadLocalRandom;
 
-
 public class RandomUtils {
-
-    static Faker faker = new Faker();
 
     public static int getRandomInt(int min, int max) {
         return ThreadLocalRandom.current().nextInt(min, max + 1);
@@ -17,24 +12,5 @@ public class RandomUtils {
         int index = getRandomInt(0, values.length - 1);
 
         return values[index];
-    }
-
-    public static String getRandomCity(String state) {
-        String city = null;
-        if (state.equals("NCR")) {
-            city = faker.options().option("Delhi", "Gurgaon", "Noida");
-        }
-        if (state.equals("Uttar Pradesh") ) {
-            city = faker.options().option("Agra", "Lucknow", "Merrut");
-        }
-        if (state.equals("Haryana") ) {
-            city =  faker.options().option("Karnal", "Panipat");
-        }
-        if (state.equals("Rajasthan") ) {
-            city = faker.options().option("Jaipur", "Jaiselmer");
-        }
-
-        return city;
-
     }
 }
